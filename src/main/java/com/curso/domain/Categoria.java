01602c983ce22d07588d01d2ec504f1d1a1ca064
MirrorRoot <mirrorsoft10@gmail.com>
2019-01-06 19:49:48 -0200

    configuraçao de banco de dados h2

--- src/main/java/com/curso/domain/Categoria.java
+++ src/main/java/com/curso/domain/Categoria.java
@@ -2,10 +2,16 @@ package com.curso.domain;
 
 import java.io.Serializable;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String nome;
 	
